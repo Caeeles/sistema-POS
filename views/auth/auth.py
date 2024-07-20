b30c5a77b26c99b9590eec34832fa879f489ed7e
Caeeles <mv.carloseduardosilva@gmail.com>
2024-07-20 13:53:37 -0300

    Adicionado as Funções dos Botões da Tela de Login

--- views/auth/auth.py
+++ views/auth/auth.py
@@ -8,3 +8,6 @@ Builder.load_file("views/auth/auth.kv")
 class Auth(BoxLayout):
     def __init__(self, **kw):
         super().__init__(**kw)
+
+    def authenticate(self):
+        App.get_running_app().root.ids.scrn_mngr.current = 'scrn_home'
