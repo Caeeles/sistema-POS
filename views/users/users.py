bef3231919522f39af9edcc3f6735de99c29981d
Caeeles <mv.carloseduardosilva@gmail.com>
2024-07-25 18:55:26 -0300

    Adicionado as páginas de Usuário, Estoque e Análises e já elaborado as funções de adicionar e modificar na seção de usuários

--- views/users/users.py
+++ views/users/users.py
@@ -1,8 +1,14 @@
-
+from datetime import datetime
+import hashlib
+from threading import Thread
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.clock import Clock
+from kivy.uix.modalview import ModalView
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.clock import Clock, mainthread
+
+from kivy.properties import StringProperty, ObjectProperty
 
 Builder.load_file('views/users/users.kv')
 
@@ -12,4 +18,135 @@ class Users(BoxLayout):
         Clock.schedule_once(self.render, .1)
 
     def render(self, _):
+        tl = Thread(target = self.get_users, daemon=True)
+        tl.start()
+
+    def add_new(self):
+        md = ModUser()
+        md.callback = self.add_user
+        md.open()
+
+    def get_users(self):
+        users = [
+                {
+                "firstName": "Carlos",
+                "lastName": "Silva",
+                "username": "caeeles",
+                "password": "102030",
+                "createdAt": "2024/01/12 12:45:00",
+                "signedIn": "2024/01/12 14:45:00"
+                },{
+                "firstName": "Bacalhau",
+                "lastName": "Silva",
+                "username": "bacalinda",
+                "password": "102030",
+                "createdAt": "2024/01/12 12:45:00",
+                "signedIn": "2024/01/12 14:45:00"
+                },{
+                "firstName": "Junior",
+                "lastName": "Silva",
+                "username": "rolalixo",
+                "password": "102030",
+                "createdAt": "2024/01/12 12:45:00",
+                "signedIn": "2024/01/12 14:45:00"
+                },
+        ]
+        self.set_users(users)
+    
+    def add_user(self, mv):
+        fname = mv.ids.fname
+        lname = mv.ids.lname
+        uname = mv.ids.uname
+        pwd = mv.ids.pwd
+        cpwd = mv.ids.cpwd
+        #verificar se os campos não estão em branco
+        if len(fname.text.strip()) < 3:
+            #informar que o fname é inválido
+            return
+        
+        _pwd = pwd.text.strip()
+        upass = hashlib.sha256(_pwd.encode()).hexdigest()
+        now = datetime.now()
+        _now = datetime.strftime(now, "%Y/%m/%d %H:%M")
+        user = {
+                "firstName": fname.text.strip(),
+                "lastName": lname.text.strip(),
+                "username": uname.text.strip(),
+                "password": upass,
+                "createdAt": _now,
+                "signedIn": "2024/01/12 14:45:00"
+            }
+        
+        self.set_users([user])
+
+    def update_user(self, user):
+        mv = ModUser()
+        mv.first_name = user.first_name
+        mv.last_name = user.last_name
+        mv.username = user.username
+        mv.callback = self.set_update
+
+        mv.open()
+
+
+    def set_update(self, mv):
+        print("Updating...")
+      
+
+    @mainthread
+    def set_users(self, users:list):
+        grid = self.ids.gl_users
+        grid.clear_widgets()
+
+        for u in users:
+            ut = UserTile()
+            ut.first_name = u['firstName']
+            ut.last_name = u['lastName']
+            ut.username = u['username']
+            ut.password = u['password']
+            ut.created = u['createdAt']
+            ut.last_login = u['signedIn']
+            ut.bind(on_release=self.update_user)
+
+            grid.add_widget(ut)
+
+
+class UserTile(ButtonBehavior, BoxLayout):
+    first_name = StringProperty("")
+    last_name = StringProperty("")
+    username = StringProperty("")
+    password = StringProperty("")
+    created = StringProperty("")
+    last_login = StringProperty("")
+    def __init__(self, **kw) -> None:
+        super().__init__(**kw)
+        Clock.schedule_once(self.render, .1)
+
+    def render(self, _):
         pass
+
+class ModUser(ModalView):
+    first_name = StringProperty("")
+    last_name = StringProperty("")
+    username = StringProperty("")
+    created = StringProperty("")
+    last_login = StringProperty("")
+    callback = ObjectProperty(allownone = True)
+    def __init__(self, **kw) -> None:
+        super().__init__(**kw)
+        Clock.schedule_once(self.render, .1)
+
+    def render(self, _):
+        pass
+
+    def on_first_name(self, inst, fname):
+        self.ids.fname.text = fname
+        self.ids.title.text = "Modificar Usuário"
+        self.ids.btn_confirm.text = "Modificar"
+        self.ids.subtitle.text = "Registre suas novas informações de usuário"
+
+    def on_last_name(self, inst, lname):
+        self.ids.lname.text = lname
+
+    def on_username(self, inst, uname):
+        self.ids.uname.text = uname
